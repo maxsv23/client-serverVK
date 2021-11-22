a4e4f736df019f006adba7749247c193ae02e037
maxsv23 <maxsv23@gmail.com>
2021-11-22 10:38:29 +0300

    add Auth VK

---
+++ cleint-Server-VK/API/Friends.swift
@@ -0,0 +1,33 @@
+//фишки xcode
+//Быстрое перемещение по файлу CTRL + 6
+// удаление пустых строк регулярное выражение (   ^\s+$   )
+//opt+cmd+ []  движение блока вверх вниз
+//cmd + -  увеличить уменьшить код
+//переименование переменной класса итд во всем коде  ПР. клавиша -> Refactor -> rename
+//Быстрый поиск cmd + shift + o
+//снипиты
+// переименовать в БЛОКЕ выделить слово - cmd + ctrl + e
+// коментарий для встроеной справки нажать на строку CMD + opt + /    тоже самое можно сделать
+/// три слеша это дескрипшен будет  в строке ниже будет описание параметров
+///
+///  DispatchQueue.main.async {
+///теперь делается в потоке main
+///    }
+//  /// MARK: - IBOUtlets
+
+//  // MARK: - Public Properties
+
+//  // MARK: - Private Properties
+
+//  // MARK: - Lifecycle
+
+//   // MARK: - Public Methods
+
+//  // MARK: - Private Methods
+
+//   // MARK: - IBActions
+import Foundation
+
+class GetFriends {
+  
+}
